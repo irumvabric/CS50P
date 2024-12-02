2be77527b611ebd53b87febea50827806bcf89ce
irumvabric <irumvabric#@gmail.com>
2024-12-02 14:48:34 +0200

    first commit

---
+++ hello.py
@@ -0,0 +1,15 @@
+# Ask user for their name
+name_input = input("What is your name? ").strip().title()
+
+# Say hello to the user
+
+
+first , last = name_input.split()
+
+print('hello,\"Friend\"' , last)
+
+
+# Ask user for their name
+
+# Say hello to the user
+ 
